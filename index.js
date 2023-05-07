9f942edb55911069f30141ec8b622e063adf8226
まっちゃてぃー <mattyacocacora0@gmail.com>
2023-05-08 08:10:16 +0900

    いろいろごにょごにょ

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-import {Misskey_Token, Misskey_Instance, api} from "./init.js";
+import {Misskey_Token, Misskey_Instance, api , boot , timeout } from "./init.js";
 import {CreateRandomNote} from "./lib/random_note.js";
 import {Notify} from "./lib/notify.js";
 
-if (Misskey_Token == undefined || Misskey_Instance == undefined) {
-    console.log("\x1b[31m[Error] Token is not set.\x1b[0m");
-    process.exit(0);
-}else{
-    const i = await api.request('i');
-    console.log(`\x1b[32m[Info] Account Name: ${JSON.stringify(i.name).replace( /"/g , "" )}`);
-    console.log(`[Info] Account ID: @${JSON.stringify(i.username).replace( /"/g , "" )}@${Misskey_Instance}\x1b[0m`);
+
+if (boot){
     Notify();
+    console.log("\x1b[32m[Info] Create Note! \n[Info] NoteId:", await CreateRandomNote());
+    setInterval(() => {
+        CreateRandomNote();
+    }, timeout);
 }
--- init.js
+++ init.js
@@ -1,11 +1,13 @@
 import * as dotenv from "dotenv";
 import * as Misskey from "misskey-js";
 import WebSocket from "ws";
+import {Notify} from "./lib/notify.js";
 dotenv.config();
+export let boot = 0;
 export const emotion_1 =["つらい","辛い","苦","くるしい","泣"];//つらみ
 export const emotion_2 =["てんさい","天才","褒めて"];//ほめられたい
-export const emotion_1_reply =["大丈夫ですか...?","よしよし...","私がぎゅーしてあげますよ..?"];
-export const emotion_2_reply =["すごいです！！","さすがです！！","天才ですね！！","すごい！！"];
+export const emotion_1_reply =["大丈夫...?","よしよし...","私がぎゅーするよ...?"];
+export const emotion_2_reply =["すごい！！","さすが！！","天才かも？？！？！！！","すごすぎる！！！！べろもぐ！！！！"];
 export const rand_vocabulary= ["うにゃーん","うにょーん","かわいいおにゃのこたべたい","べろべろべろべろべろべろ","もぐ...","かわいいおにゃのこおいしかった"]
 export const Misskey_Token= process.env.TOKEN;
 export const Misskey_Instance= process.env.INSTANCE;
@@ -13,7 +15,28 @@ export const api = new Misskey.api.APIClient({
     origin: "https://"+Misskey_Instance,
     credential: Misskey_Token,
 });
-export const stream = new Misskey.Stream("https://"+Misskey_Instance, { token: Misskey_Token }, {WebSocket: WebSocket});
+export const timeout = process.env.TIMEOUT * 60 * 1000;
+
+console.log("\x1b[32m              __  __            __           _______             ____\n" +
+    "  __ _  ___ _/ /_/ /___ _____ _/ /____ ___ _/ ___/ /  ___ ____  / / /\n" +
+    " /  ' \\/ _ `/ __/ __/ // / _ `/ __/ -_) _ `/ /__/ _ \\/ _ `/ _ \\/_/_/ \n" +
+    "/_/_/_/\\_,_/\\__/\\__/\\_, /\\_,_/\\__/\\__/\\_,_/\\___/_//_/\\_,_/_//_(_|_)  \n" +
+    "                   /___/                                             \n");
+
+
+
+export const stream = new Misskey.Stream("https://" + Misskey_Instance, {token: Misskey_Token}, {WebSocket: WebSocket});
 stream.on('_connected_', () => {
     console.log('\x1b[32m[Info] WebSocket Connected');
 });
+    if (Misskey_Token == undefined || Misskey_Instance == undefined) {
+        console.log("\x1b[31m[Error] Token is not set.\x1b[0m");
+        process.exit(0);
+    } else {
+        const i = await api.request('i');
+        console.log(`\x1b[32m[Info] Account Name: ${JSON.stringify(i.name).replace(/"/g, "")}`);
+        console.log(`[Info] Account ID: @${JSON.stringify(i.username).replace(/"/g, "")}@${Misskey_Instance}\x1b[0m`);
+        boot = 1;
+
+}
+
