7e6133cf52e18cbd68359140324c4e05b373b9b5
まっちゃてぃー <mattyacocacora0@gmail.com>
2023-05-08 09:11:36 +0900

    気圧のやつつく多ｔ

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import {Misskey_Token, Misskey_Instance, api , boot , timeout } from "./init.js";
 import {CreateRandomNote} from "./lib/random_note.js";
 import {Notify} from "./lib/notify.js";
-
+import {pressure} from "./lib/pressure.js";
 
 if (boot){
     Notify();
+    pressure()
     setInterval(() => {
         CreateRandomNote();
     }, timeout);
